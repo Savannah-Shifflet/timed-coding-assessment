2dd90b85c9087594408c946b323c4a3a8e3d63e4
Savannah Shifflet <savannah.shifflet@gmail.com>
2023-07-06 17:37:25 -0500

    added timer function to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,28 @@
         // four answer choices as buttons appear
         // when the button is clicked, it confirms if correct or incorrect (set data attribute to "correct" or "incorrect" for each answer?)    
     //if an answer is answered incorrectly, subtract time from clock 
-    // end quiz when time is up or questions run out 
+    
+    var timeEl = document.querySelector("#timer");
+    
+    // Set timer to 50 seconds for the user (extra second to accommodate loading)
+    var secondsLeft = 51; 
+    
+    // Create timer function for when the start button is pressed
+    function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = "Time: " + secondsLeft;
+    
+        if(secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // end quiz when time is up or questions run out 
+            // TODO: end quiz and call highscore entry function
+        }
+
+    }, 1000);
+    }
 
 // store scores in local storage 
     // key: 1,2,3, etc. 
