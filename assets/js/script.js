9f6acfba85d4b984cfab80db0cc43b1627e69c87
Savannah Shifflet <savannah.shifflet@gmail.com>
2023-07-06 10:03:57 -0500

    added pseudocode to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//  timer function 
+    // quiz function
+        // question appears 
+        // four answer choices as buttons appear
+        // when the button is clicked, it confirms if correct or incorrect (set data attribute to "correct" or "incorrect" for each answer?)    
+    //if an answer is answered incorrectly, subtract time from clock 
+    // end quiz when time is up or questions run out 
+
+// store scores in local storage 
+    // key: 1,2,3, etc. 
+    // data: SS - score
+
+// start game function: start page with start button
+    // when button is selected, the start button/info hides
+    // timer function is called 
+    // score storage function is called 
