4e94771e2eb88fe48a81715f50801f0aadd495bd
Savannah Shifflet <savannah.shifflet@gmail.com>
2023-07-06 17:54:15 -0500

    created start quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
-//  timer function 
-    // quiz function
+var startPrompt = document.querySelector("#start")
+var questionContainer = document.querySelector("#questionContainer");
+// quiz function
+    function runQuiz() {
+        
+        
+
+    }
         // question appears 
         // four answer choices as buttons appear
         // when the button is clicked, it confirms if correct or incorrect (set data attribute to "correct" or "incorrect" for each answer?)    
     //if an answer is answered incorrectly, subtract time from clock 
+
+// Identifies the timer element in the HTML 
+var timeEl = document.querySelector("#timer");
     
-    var timeEl = document.querySelector("#timer");
-    
-    // Set timer to 50 seconds for the user (extra second to accommodate loading)
-    var secondsLeft = 51; 
+// Set timer to 50 seconds for the user (extra second to accommodate loading)
+var secondsLeft = 51; 
     
     // Create timer function for when the start button is pressed
     function setTime() {
@@ -30,6 +37,16 @@
 // store scores in local storage 
     // key: 1,2,3, etc. 
     // data: SS - score
+
+function startQuiz() {
+    startPrompt.setAttribute("style", "display: none"); 
+    questionContainer.setAttribute("style", "display: block");
+    setTime(); 
+    runQuiz();
+}
+
+var startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", startQuiz);
 
 // start game function: start page with start button
     // when button is selected, the start button/info hides
